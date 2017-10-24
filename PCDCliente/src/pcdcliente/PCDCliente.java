7e3743ccef4098b0d3b12f7ddd6a9cd1a1b205d6
alvaroalmanza88 <alvaro.almanza88@gmail.com>
2017-10-24 18:48:19 +0200

    Creacion Proyecto Base
    Se crea base del proyecto para empezar a trabajar de manera colaborativa en el grupo.

---
+++ PCDCliente/src/pcdcliente/PCDCliente.java
@@ -0,0 +1,20 @@
+/*
+ * CODIGO REALIZADO POR ALVARO ALMANZA.
+ * UNIVERSIDAD INTERNACIONAL I DE CASTILLA
+ */
+package pcdcliente;
+
+/**
+ *
+ * @author alvar
+ */
+public class PCDCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
