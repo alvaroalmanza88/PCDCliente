5fb38d5175c0320aa6deeaba1049c80aecc52efc
alvaroalmanza88 <alvaro.almanza88@gmail.com>
2017-10-24 20:01:49 +0200

    Implementacion de Interfaz Grafica
    Se crea una clase adicional, un JForm con el diseño visual de la aplicacion y en la clase principal se crea un objeto de dicha clase para mostrarla por pantalla

--- PCDCliente/src/pcdcliente/PCDCliente.java
+++ PCDCliente/src/pcdcliente/PCDCliente.java
@@ -4,14 +4,6 @@
  */
 package pcdcliente;
 
-// Añadido por Jose Manuel Serrano
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-
-
 /**
  *
  * @author alvar
@@ -21,39 +13,13 @@ public class PCDCliente {
     /**
      * @param args the command line arguments
      */
-    // Añadido por Jose Manuel Serrano
-    // HE AÑADIDO TRES CAMPOS UNO PARA EL NICK, OTRO PARA LA IP DE LA CONEXION Y OTRO PARA EL MENSAJE, JUNTO CON EL BOTON ENVIAR
-    txtnick=new JTextField();
-        txtnick.setBounds(10,10,400,25);
-        txtip=new JTextField();
-        txtip.setBounds(10,30,400,25);
-        txtmensaje=new JTextField();
-        txtmensaje.setBounds(10,50,400,25);
-        btnenviar=new JButton("Enviar datos");
-        btnenviar.setBounds(10,70,400,25);
-        btnenviar.addActionListener(this);
-        add(txtnick);
-        add(txtip);
-        add(txtmensaje);
-        add(btnenviar);
-        setLayout (null);
-        setSize(500,500);
-        setVisible(true);
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
+        Principal ventana= new Principal();
+        ventana.setVisible(true);
+        ventana.setResizable(false);
+        ventana.setLocationRelativeTo(null);
     }
-    new PCDCliente();
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == btnenviar){
-            try{
-                
-            }catch (Exception ex){
-                System.out.println(ex.getMessage());
-            }
-        }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+
 }
