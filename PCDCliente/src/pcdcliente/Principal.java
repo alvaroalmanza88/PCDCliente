d2d95ab786bfcf682a1b2d442e845c6ccf397125
alvaroalmanza88 <alvaro.almanza88@gmail.com>
2017-10-26 20:08:40 +0200

    Implementaciones varias
    1- Metodo de comprobacion de ip por medio de refex
    2. Metodo de comprobacion de rango de puerto valido
    3- Accion del boton "conectar" al servidor
    4- Accion del boton "Seleccionar" para escoger el archivo a enviar

--- PCDCliente/src/pcdcliente/Principal.java
+++ PCDCliente/src/pcdcliente/Principal.java
@@ -4,7 +4,10 @@
  */
 package pcdcliente;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
@@ -12,6 +15,11 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.text.Utilities;
 
 /**
  *
@@ -28,8 +36,13 @@ public class Principal extends javax.swing.JFrame {
     String datos;
     String direccion;
     int puerto;
+    boolean ipok;
+    boolean puertook;
+    BufferedInputStream bis;
+    BufferedOutputStream bos;
     public Principal() {
         initComponents();
+        jButton2.setEnabled(false);
     }
 
     /**
@@ -47,6 +60,8 @@ public class Principal extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jTextField3 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -62,12 +77,19 @@ public class Principal extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Seleccionar Archivo");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 24, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel3)
@@ -79,7 +101,13 @@ public class Principal extends javax.swing.JFrame {
                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(38, 38, 38)
                         .addComponent(jButton1)))
-                .addGap(73, 73, 73))
+                .addGap(130, 130, 130))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -93,7 +121,11 @@ public class Principal extends javax.swing.JFrame {
                     .addComponent(jLabel4)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(80, Short.MAX_VALUE))
         );
 
         jLabel1.setText("Programa Cliente");
@@ -118,24 +150,58 @@ public class Principal extends javax.swing.JFrame {
                 .addGap(8, 8, 8)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        JFileChooser fc = new JFileChooser();
+        int respuesta = fc.showOpenDialog(this);
+        if(respuesta == JFileChooser.APPROVE_OPTION)
+        {
+            File seleccion = fc.getSelectedFile();
+            jTextField1.setText(fc.getName(seleccion));
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         //implementacion del codigo
-        direccion=jTextField3.getText();
-        puerto=Integer.parseInt(jTextField2.getText());
-        try{
-                    yo = new Socket(direccion,puerto);
-   		}
+        ipok=ipvalida(jTextField3.getText());
+        if (ipok)
+        {
+            direccion=jTextField3.getText();
+        }else
+        {
+            JOptionPane.showMessageDialog(this,"No Ha introducido una direccion valida");
+            jTextField3.setText("");
+            jTextField2.setText("");
+        }
+        puertook=puertoValido(Integer.parseInt(jTextField2.getText()));
+        if(puertook)
+        {
+            puerto=Integer.parseInt(jTextField2.getText());
+            try
+            {
+                yo = new Socket(direccion,puerto);
+                JOptionPane.showMessageDialog(this,"Se ha Conectado correctamente al servidor");
+                jButton2.setEnabled(true);
+                jButton1.setEnabled(false);
+            }
             catch (UnknownHostException e)
             {
    		System.out.println(e.getMessage());
-            } catch (IOException ex) {
-            Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                JOptionPane.showMessageDialog(this,"Host desconocido");
+            } catch (IOException ex) 
+            {
+                JOptionPane.showMessageDialog(this,"Se ha detectado un error, vuelva a intentarlo de nuevo");
+                Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+                
+            }
+            
         }
     }
 
@@ -176,11 +242,36 @@ public class Principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
+    public static boolean ipvalida(String ipAddr)
+    {
+        boolean valida=false;
+        if (ipAddr.equals("localhost"))
+        {
+            valida=true;
+        }else
+        { 
+            Pattern ptn = Pattern.compile("^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$");
+            Matcher mtch = ptn.matcher(ipAddr);
+            valida= mtch.find();
+        }
+        return valida;
+    }
+    public static boolean puertoValido(int puerto)
+    {
+        boolean valido=false;
+        if (1024 <= puerto && 65535 >= puerto)
+        {
+            valido=true;
+        }
+        return valido;
+    }
 }
